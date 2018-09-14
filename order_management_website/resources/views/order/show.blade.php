964c12086f49bfa35cfb29c86437264e473ed4a4
Rory <wehdy224@gmail.com>
2018-09-14 11:40:49 +0800

    add case list on order show page

--- order_management_website/resources/views/order/show.blade.php
+++ order_management_website/resources/views/order/show.blade.php
@@ -41,6 +41,39 @@
     <dt class="col-md-3">{{ __('order.fields.remark')}}</dt>
     <dd class="col-md-9">{{ $order->remark? $order->remark:'-' }}</dd>
 </dl>
+<div class="row">
+    @foreach($order->cases as $case)
+        <div class="col-12 col-md-6 mb-3 mb-md-4">
+            <div class="card h-100">
+                <div class="card-body d-flex flex-column">
+                    <div class="d-flex flex-column flex-md-row align-items-center">
+                        <h5 class="card-title">{{$case->case_type_name}}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted ml-md-auto">
+                                <span class="fa fa-dollar mr-1"></span>{{$case->price? $case->price:0 }}
+                                <span class="fa fa-archive mr-1"></span>{{$case->amount? $case->amount:0}}
+                        </h6>
+                    </div>
+                    @if(!count($case->cookies))
+                        <div class="mt-2 text-center text-secondary mt-auto mb-auto">
+                            <span class="fa fa-exclamation-triangle font-size-60"></span>
+                            <div class="card-subtitle">尚未選擇禮盒內容</div>
+                        </div>
+                    @else
+                        @foreach($case->cookies as $cookie)
+                            <div class="mt-2">
+                                <span>{{$cookie->cookie_name}}</span>
+                                <span class="float-right">
+                                {{$cookie->pack_name}}
+                                <span class="mx-2">X</span>
+                                {{$cookie->amount?$cookie->amount:0}}</span>
+                            </div>
+                        @endforeach
+                    @endif
+                </div>
+            </div>
+        </div>
+    @endforeach
+</div>
 @endsection
 
 @section('custom-js')
