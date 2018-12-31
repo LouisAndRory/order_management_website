7478d36e8f953c44ae75f8a8b1e5fe90299b1f79
Louis Hong <ts00100665@gamil.com>
2018-12-31 23:26:06 +0800

    Update pdf config.

--- order_management_website/config/snappy.php
+++ order_management_website/config/snappy.php
@@ -1,18 +1,22 @@
 <?php
 
 return array(
+
+
     'pdf' => array(
         'enabled' => true,
-        'binary'  => env('PDF_BIN_PATH', app_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64')),
+        'binary'  => env('PDF_BIN_PATH', '/usr/local/bin/wkhtmltopdf'),
         'timeout' => false,
         'options' => array(),
         'env'     => array(),
     ),
     'image' => array(
         'enabled' => true,
-        'binary'  => env('IMAGE_BIN_PATH', app_path('vendor/h4cc/wkhtmltoimage-amd64/bin/wkhtmltoimage-amd64')),
+        'binary'  => env('IMAGE_BIN_PATH', '/usr/local/bin/wkhtmltoimage'),
         'timeout' => false,
         'options' => array(),
         'env'     => array(),
     ),
+
+
 );
