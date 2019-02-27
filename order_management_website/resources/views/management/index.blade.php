36c4b6dd2612b14ce693c8f73018ee6f805e071e
Rory <wehdy224@gmail.com>
2019-02-27 18:18:44 +0800

    update style

--- order_management_website/resources/views/management/index.blade.php
+++ order_management_website/resources/views/management/index.blade.php
@@ -5,21 +5,21 @@
     <div class="col-12 col-md-4 col-lg-3 mb-3">
         <a class="d-flex flex-column justify-content-center align-items-center bg-white" href="{{ route('cookie.index')}}">
             <img src="{{ asset('images/cookie.png') }}" alt="" class="h-auto w-50 py-3">
-            <button class="btn bg-main text-white w-100">{{ __('navigation.options.cookie') }}</button>
+            <div class="bg-main py-2 text-center text-white w-100">{{ __('navigation.options.cookie') }}</div>
         </a>
     </div>
 
     <div class="col-12 col-md-4 col-lg-3 mb-3">
         <a class="d-flex flex-column justify-content-center align-items-center bg-white" href="{{ route('caseType.index')}}">
             <img src="{{ asset('images/case.png') }}" alt="" class="h-auto w-50 py-3">
-            <button class="btn bg-main text-white w-100">{{ __('navigation.options.case') }}</button>
+            <div class="bg-main py-2 text-center text-white w-100">{{ __('navigation.options.case') }}</div>
         </a>
     </div>
 
     <div class="col-12 col-md-4 col-lg-3 mb-3">
         <a class="d-flex flex-column justify-content-center align-items-center bg-white" href="{{ route('pack.index')}}">
             <img src="{{ asset('images/packing.png') }}" alt="" class="h-auto w-50 py-3">
-            <button class="btn bg-main text-white w-100">{{ __('navigation.options.pack') }}</button>
+            <div class="bg-main py-2 text-center text-white w-100">{{ __('navigation.options.pack') }}</div>
         </a>
     </div>
 </div>
